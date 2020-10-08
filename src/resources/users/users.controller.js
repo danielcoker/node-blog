f41926738c473e4ace6a39538e91429b0976b74a
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-10-08 16:45:10 +0100

    Add register controller and service

--- src/resources/users/users.controller.js
+++ src/resources/users/users.controller.js
@@ -1,6 +1,9 @@
 import asyncHandler from '../../libs/asyncHanlder';
+import * as UserService from './users.service';
 
 // eslint-disable-next-line import/prefer-default-export
 export const register = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Registration successful.' });
+  const user = await UserService.register(req.body);
+
+  res.status(200).json({ message: 'Registration successful.', data: { user } });
 });
