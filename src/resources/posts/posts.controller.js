da4cec05ef8ef7fc87e4aea0e71060a62a53ac25
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-10-10 11:13:37 +0100

    Add get post endpoint

--- src/resources/posts/posts.controller.js
+++ src/resources/posts/posts.controller.js
@@ -1,6 +1,13 @@
 import asyncHandler from '../../libs/asyncHanlder';
 import * as PostService from './posts.service';
 
+export const getPost = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+  const post = await PostService.getPost(postId);
+
+  res.respond(200, { post }, 'Post retrieved successfully.');
+});
+
 export const createPost = asyncHandler(async (req, res) => {
   const post = await PostService.createPost(req.user, req.body);
 
